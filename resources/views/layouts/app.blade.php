90655fc0e80beeff31c3fc2bf8b4505b0d0f0b8d
noters-film <ec2-user@ip-172-31-37-171.ap-northeast-1.compute.internal>
2021-12-29 10:23:11 +0000

    08 add footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,6 +28,8 @@
         <main class="py-4 mb-5">
             @yield('content')
         </main>
+                @component('components.footer')
+                @endcomponent
     </div>
 </body>
 </html>
