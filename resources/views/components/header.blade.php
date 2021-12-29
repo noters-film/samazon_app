89cf1d31732c8c7d58d8631c7b994c5b057a2edd
noters-film <ec2-user@ip-172-31-37-171.ap-northeast-1.compute.internal>
2021-12-29 15:32:11 +0000

    15 userinfo editing

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,38 +1,42 @@
-<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-    <a class="navbar-brand ml-4" href="{{ url('/') }}">
+<nav class="navbar navbar-expand-md navbar-light shadow-sm samazon-header-container">
+    <a class="navbar-brand" href="{{ url('/') }}">
         <img src="{{asset('img/logo.jpg')}}">
     </a>
+    <form class="form-inline">
+        <div class="form-group">
+            <input class="form-control samazon-header-search-input">
+        </div>
+        <div class="input-group">
+            <button type="submit" class="btn samazon-header-search-button"><i class="fas fa-search samazon-header-search-icon"></i></button>
+        </div>
+    </form>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
         <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <!-- Right Side Of Navbar -->
-        <ul class="navbar-nav ml-auto mr-5">
+        <ul class="navbar-nav ml-auto mr-5 mt-2">
             <!-- Authentication Links -->
             @guest
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('register') }}">新規登録</a>
+            <li class="nav-item mr-5">
+                <a class="nav-link" href="{{ route('register') }}"><label>新規登録</label></a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('login') }}">ログイン</a>
+            <li class="nav-item mr-5">
+                <a class="nav-link" href="{{ route('login') }}"><label>ログイン</label></a>
+            </li>
+            <hr>
+            <li class="nav-item mr-5">
+                <a class="nav-link" href="{{ route('login') }}"><i class="far fa-heart"></i></a>
+            </li>
+            <li class="nav-item mr-5">
+                <a class="nav-link" href="{{ route('login') }}"><i class="fas fa-shopping-cart"></i></a>
             </li>
             @else
-            <li class="nav-item dropdown">
-                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                    {{ Auth::user()->name }} <span class="caret"></span>
+            <li class="nav-item mr-5">
+                <a class="nav-link" href="{{ route('mypage') }}">
+                    <i class="fas fa-user mr-1"></i><label>マイページ</label>
                 </a>
-
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                            document.getElementById('logout-form').submit();">
-                        {{ __('Logout') }}
-                    </a>
-
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        @csrf
-                    </form>
-                </div>
             </li>
             @endguest
         </ul>
--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -6,7 +6,7 @@
 @if ($level === 'error')
 # @lang('Whoops!')
 @else
-# @lang('Hello!')
+<b>こんにちは</b>
 @endif
 @endif
 
@@ -43,7 +43,6 @@
 @if (! empty($salutation))
 {{ $salutation }}
 @else
-@lang('Regards'),<br>
 {{ config('app.name') }}
 @endif
 
@@ -51,8 +50,8 @@
 @isset($actionText)
 @slot('subcopy')
 @lang(
-    "If you’re having trouble clicking the \":actionText\" button, copy and paste the URL below\n".
-    'into your web browser: [:actionURL](:actionURL)',
+":actionText ボタンが利用できない場合は、以下のURLをコピー＆ペーストしてブラウザから直接アクセスしてください。<br />\n" .
+    '[:actionURL](:actionURL)',
     [
         'actionText' => $actionText,
         'actionURL' => $actionUrl,
