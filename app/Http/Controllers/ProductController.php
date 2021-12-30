17ccb6be072f3d786d3c1b32ab067a548ab7be01
noetrs-film <takaboo07181994@gmail.com>
2021-12-30 10:19:12 +0000

    2-8 create manage4

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -27,30 +27,28 @@ class ProductController extends Controller
             $sort_query[$slices[0]] = $slices[1];
             $sorted = $request->sort;
         }
+
         if ($request->category !== null) {
             $products = Product::where('category_id', $request->category)->sortable($sort_query)->paginate(15);
             $total_count = Product::where('category_id', $request->category)->count();
             $category = Category::find($request->category);
         } else {
-            $products = Product::sortable($sort_query)->paginate(15);
+            $products = Product::paginate(15);
             $total_count = "";
             $category = null;
         }
-        
+
         $sort = [
-            '並び替え' => '', 
             '価格の安い順' => 'price asc',
-            '価格の高い順' => 'price desc', 
-            '出品の古い順' => 'updated_at asc', 
+            '価格の高い順' => 'price desc',
+            '出品の古い順' => 'updated_at asc',
             '出品の新しい順' => 'updated_at desc'
         ];
 
         $categories = Category::all();
-
         $major_category_names = Category::pluck('major_category_name')->unique();
 
         return view('products.index', compact('products', 'category', 'categories', 'major_category_names', 'total_count', 'sort', 'sorted'));
-
     }
 
     public function favorite(Product $product)
@@ -65,37 +63,6 @@ class ProductController extends Controller
 
         return redirect()->route('products.show', $product);
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        $categories = Category::all();
-
-        return view('products.create', compact('categories'));
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $product = new Product();
-        $product->name = $request->input('name');
-        $product->description = $request->input('description');
-        $product->price = $request->input('price');
-        $product->category_id = $request->input('category_id');
-        $product->save();
-
-        return redirect()->route('products.show', ['id' => $product->id]);
-    }
-
     /**
      * Display the specified resource.
      *
@@ -109,49 +76,4 @@ class ProductController extends Controller
         return view('products.show', compact('product', 'reviews'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Product $product)
-    {
-        $categories = Category::all();
-
-        return view('products.edit', compact('product', 'categories'));
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Product $product)
-    {
-        $product->name = $request->input('name');
-        $product->description = $request->input('description');
-        $product->price = $request->input('price');
-        $product->category_id = $request->input('category_id');
-        $product->update();
-
-        return redirect()->route('products.show', ['id' => $product->id]);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Product $product)
-    {
-        $product->delete();
-
-        $reviews = $product->reviews()->get();
-
-        return view('products.show', compact('product', 'reviews'));
-    }
 }
