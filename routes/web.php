2820315c0eea3e2c2918cb4a2b2c1902a945e40c
noetrs-film <takaboo07181994@gmail.com>
2021-12-30 12:51:11 +0000

    2-19 create manage9

--- routes/web.php
+++ routes/web.php
@@ -44,6 +44,8 @@ Route::group(['prefix' => 'dashboard', 'as' => 'dashboard.'], function() {
     Route::resource('products', 'Dashboard\ProductController')->middleware('auth:admins');
     Route::resource('major_categories', 'Dashboard\MajorCategoryController')->middleware('auth:admins');
     Route::resource('users', 'Dashboard\UserController')->middleware('auth:admins');
+    
+    Route::get('orders', 'Dashboard\OrderController@index')->middleware('auth:admins');
 });
 
 if (env('APP_ENV') === 'production') {
